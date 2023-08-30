7b0d85d965498a7b66937187ad6c2ef6cfcb59d0
master <vogusov@gmail.com>
2023-08-31 00:04:15 +0300

    Logger

---
+++ Src/Logger/LogLevel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BugReport\Logger;
+
+
+/**
+ * PSR-3
+ */
+class LogLevel
+{
+    const DEBUG = 'debug';
+    const INFO = 'info';
+    const NOTICE = 'notice';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+    const CRITICAL = 'critical';
+    const ALERT = 'alert';
+    const EMERGENCY = 'emergency';
+}
--- index.php
+++ index.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 use BugReport\Helpers\Config;
 use BugReport\Helpers\App;
+use BugReport\Logger\Logger;
 
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/Src/Exception/exception.php";
 
-set_error_handler([new \BugReport\Exception\ExceptionHandler(), 'convertWarningsAndNoticesToException']);
-set_exception_handler([new \BugReport\Exception\ExceptionHandler(), 'handle']);
+// set_error_handler([new \BugReport\Exception\ExceptionHandler(), 'convertWarningsAndNoticesToException']);
+// set_exception_handler([new \BugReport\Exception\ExceptionHandler(), 'handle']);
 
-// $config = Config::getFileContent('app');
-// var_dump($config);
 
-$db = new mysqli('dafbdb', 'root', 'root', 'bug');
+$logger = new Logger();
+$logger->log('emergency', 'test EMERGENCE level', ['DATATA' => 'LVCPVCPWYDPIVCYP']);
+$logger->info('User ass created sucks', ['id' => 666]); 
